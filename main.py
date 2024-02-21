9730cbd8132065ec39cbb46482ca36b681ec7fb4
Catherine Hughes <catherhu@uio.no>
2024-02-21 15:26:53 +0100

    added a draft for velocity verlet

--- main.py
+++ main.py
@@ -6,6 +6,18 @@ argon_atom = md.particle(40, np.array([0,0,0]), np.array([1,0,0]))
 my_system = md.system(1)
 my_system.add_particle(argon_atom)
 
+#testing
+for particle in my_system.particles:
+    print(particle.m)
+    print(particle.r)
+    print(particle.v)
+
+t = 1
+dt = 0.001
+
+my_system.verlet_evolve(dt)
+
+#testing
 for particle in my_system.particles:
     print(particle.m)
     print(particle.r)
--- md.py
+++ md.py
@@ -11,3 +11,13 @@ class system:
 
     def add_particle(self, particle):
         self.particles.append(particle)
+
+    def total_force(self):
+        return 0
+
+    def verlet_evolve(self, dt):
+        a = self.total_force()/self.particles[0].m
+        self.particles[0].v = self.particles[0].v + 0.5*a*dt
+        self.particles[0].r = self.particles[0].r + self.particles[0].v*dt
+        a = self.total_force()/self.particles[0].m
+        self.particles[0].v = self.particles[0].v + 0.5*a*dt
